77f9fbe776153d2212547b915c18b864d1443d5c
Daniel Aguirre Correa <danaguir@WKMZTACAFBCF.global.publicisgroupe.net>
2023-09-07 12:11:43 -0500

    feature: changed db provider to mongodb

--- src/services/rule.service.ts
+++ src/services/rule.service.ts
@@ -1,58 +1,26 @@
-import { PrismaClient } from "@prisma/client";
-import { RuleModel } from "../models/rule.model";
+import { PrismaClient, Rule } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
 export default class RuleService {
-  static getAllRules = async (): Promise<RuleModel[]> => {
+  static getAllRules = async (): Promise<Rule[]> => {
     const rules = await prisma.rule.findMany({
       include: {
-        markets: true
+        validation: true
       }
     })
     return rules
   }
 
-  static addNewRule = async (rule: RuleModel) => {
-    const ruleCreated = await prisma.rule.create({
-      data: {
-        name: rule.name,
-        description: rule.description,
-        type: rule.type,
-        markets: {
-          create: rule.markets
-        }
-      },
-      include: {
-        markets: true
-      }
-    })
-    return ruleCreated
+  static addNewRule = async (rule: Rule) => {
+   
   }
 
-  static updateRule = async (rule: RuleModel) => {
-    const markets = rule.markets.map((market) => ({
-      where: { id: market.id },
-      data: market
-    }))
-    const ruleUpdated = await prisma.rule.update({
-      where: { id: rule.id },
-      data: {
-        name: rule.name,
-        description: rule.description,
-        type: rule.type,
-        markets: {
-          update: markets
-        }
-      }
-    })
-    return ruleUpdated
+  static updateRule = async (rule: Rule) => {
+   
   }
 
   static deleteRule = async (ruleId: number) => {
-    const deletedRule = await prisma.rule.delete({
-      where: { id: ruleId },
-    })
-    return deletedRule
+  
   }
 }
